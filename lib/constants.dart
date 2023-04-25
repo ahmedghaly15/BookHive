155695b35b2cad4cafe7776940dd471d3753e700
ahmed ghaly <ahmedghaly0767@gmail.com>
2023-04-25 20:02:18 +0200

    Refactor initState % Achieve navigate to Home

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xff102e44);
+
+const Duration kTransitionDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,4 +1,7 @@
+import 'package:book_hive/constants.dart';
+import 'package:book_hive/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../../../core/utils/assets_data.dart';
 
@@ -12,23 +15,15 @@ class SplashViewBody extends StatefulWidget {
 class _SplashViewBodyState extends State<SplashViewBody>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
-  late Animation<double> slideAnimation;
+  late Animation<double> fadeAnimation;
 
   @override
   void initState() {
     super.initState();
 
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
+    initFadingAnimation();
 
-    slideAnimation = Tween<double>(
-      begin: 0,
-      end: 1,
-    ).animate(animationController);
-
-    animationController.forward();
+    navigateToHome();
   }
 
   @override
@@ -42,14 +37,41 @@ class _SplashViewBodyState extends State<SplashViewBody>
     return Center(
       // To rebuild only this widget
       child: AnimatedBuilder(
-        animation: slideAnimation,
+        animation: fadeAnimation,
         builder: (context, _) => FadeTransition(
-          opacity: slideAnimation,
+          opacity: fadeAnimation,
           child: Image.asset(
             AssetsData.logo,
           ),
         ),
       ),
+    );
+  }
+
+  void initFadingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    fadeAnimation = Tween<double>(
+      begin: 0,
+      end: 1,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(
+      const Duration(milliseconds: 3500),
+      () {
+        Get.off(
+          () => const HomeView(),
+          transition: Transition.fade,
+          duration: kTransitionDuration,
+        );
+      },
     );
   }
 }

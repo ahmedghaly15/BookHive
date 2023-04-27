cf75242bcd2a548f2a51c241ad725f8b7d18970d
ahmed ghaly <ahmedghaly0767@gmail.com>
2023-04-27 08:57:27 +0200

    Create home_repo methods & failure file

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:book_hive/core/errors/failure.dart';
+import 'package:book_hive/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+}

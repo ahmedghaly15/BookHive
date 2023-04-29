71526a0b437e0e01798c75eab81cb6c6d7c1e2e8
ahmed ghaly <ahmedghaly0767@gmail.com>
2023-04-29 13:01:58 +0300

    Add Book Description to Book Details View

--- lib/features/home/presentation/views/widgets/book_details_section.dart
+++ lib/features/home/presentation/views/widgets/book_details_section.dart
@@ -55,8 +55,37 @@ class BookDetailsSection extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
         ),
         const SizedBox(height: 25),
+        if (book.volumeInfo.description != null)
+          DescriptionContainer(book: book),
+        const SizedBox(height: 25),
         BookActions(book: book),
       ],
+    );
+  }
+}
+
+class DescriptionContainer extends StatelessWidget {
+  final BookModel book;
+  const DescriptionContainer({super.key, required this.book});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: kDefaultPadding / 2,
+        horizontal: kDefaultPadding,
+      ),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 7, 33, 54),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Text(
+        book.volumeInfo.description!,
+        maxLines: 5,
+        overflow: TextOverflow.ellipsis,
+        textAlign: TextAlign.justify,
+        style: Styles.textStyle14.copyWith(height: 1.5),
+      ),
     );
   }
 }
